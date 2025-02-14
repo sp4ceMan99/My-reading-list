5f8439e2a79f515ff9e8ca11eb2165f1eba235c2
sp4ceMan99 <fbeyrand@outlook.com>
2025-02-14 22:44:27 +0100

    Update

--- src/js/theme.js
+++ src/js/theme.js
@@ -10,13 +10,7 @@ browser.storage.sync.get("theme").then((data) => {
     setTheme(currentTheme);
 });
 
-const saveTheme = (theme) => {
-  browser.storage.sync.set({ theme: theme }).then((data) => {
-    console.log("Theme set in sync :", theme);
-  });
-}
-
-const setTheme = (theme) => {
+function setTheme(theme) {
   document.documentElement.setAttribute("data-theme", theme);
   updateIcons(theme);
   currentTheme = theme;
@@ -28,8 +22,11 @@ function updateIcons(theme) {
 }
 
 themeToggleButton.addEventListener("click", () => {
-  setTheme(currentTheme === "light" ? "dark" : "light");
-  saveTheme(currentTheme);
+  const theme = currentTheme === "light" ? "dark" : "light"
+  setTheme(theme);
+  browser.storage.sync.set({ theme: theme }).then(() => {
+    console.log("Theme set in sync :", theme);
+  });
 });
 
 browser.storage.onChanged.addListener((changes, area) => {
